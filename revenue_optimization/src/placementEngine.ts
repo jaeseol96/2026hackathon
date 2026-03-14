4936d40a4af41e6bc0aef49c2f9dead24403db69
Jae Seol <jae_seol@hotmail.com>
2026-03-14 12:54:02 -0500

    Placement engine

--- revenue_optimization/src/placementEngine.ts
+++ revenue_optimization/src/placementEngine.ts
@@ -57,13 +57,24 @@ export class PlacementEngine {
         area: Area,
         startTime: number
     ): boolean {
-        return (!(startTime < ad.timeReceived)
-            && (startTime + ad.duration < ad.timeout)
-            && (startTime - ad.timeReceived <= ad.timeout)
-            && (ad.duration <= area.timeWindow));
+        const placementEndTime = startTime + ad.duration;
+
+        return (
+            startTime >= ad.timeReceived
+            && startTime <= ad.timeReceived + ad.timeout
+            && placementEndTime <= area.timeWindow
+        );
     }
 
     isAdAlreadyScheduled(adId: string, schedule: Schedule): boolean {
+        for (const areaSchedule of Object.values(schedule)) {
+            for (const scheduledAd of areaSchedule) {
+                if (scheduledAd.adId === adId) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -73,10 +84,65 @@ export class PlacementEngine {
         schedule: Schedule,
         startTime: number
     ): boolean {
-        return false;
+        if (!this.isAdCompatibleWithArea(ad, area)) {
+            return false;
+        }
+        if (this.isAdAlreadyScheduled(ad.adId, schedule)) {
+            return false;
+        }
+        if (!this.doesPlacementFitTimingConstraints(ad, area, startTime)) {
+            return false;
+        }
+
+        const areaSchedule = schedule[area.areaId] ?? [];
+        const placementEndTime = startTime + ad.duration;
+
+        for (const scheduledAd of areaSchedule) {
+            const overlapsExisting =
+                startTime < scheduledAd.endTime
+                && placementEndTime > scheduledAd.startTime;
+            if (overlapsExisting) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     isAreaScheduleValid(area: Area, areaSchedule: ScheduledAd[], ads: Ad[]): boolean {
-        return false;
+        const adById: Record<string, Ad> = {};
+        for (const ad of ads) {
+            adById[ad.adId] = ad;
+        }
+
+        for (const scheduledAd of areaSchedule) {
+            const ad = adById[scheduledAd.adId];
+            if (!ad) {
+                return false;
+            }
+            if (scheduledAd.areaId !== area.areaId) {
+                return false;
+            }
+
+            const scheduledDuration = scheduledAd.endTime - scheduledAd.startTime;
+            if (scheduledDuration !== ad.duration) {
+                return false;
+            }
+            if (!this.isAdCompatibleWithArea(ad, area)) {
+                return false;
+            }
+            if (!this.doesPlacementFitTimingConstraints(ad, area, scheduledAd.startTime)) {
+                return false;
+            }
+        }
+
+        const sortedSchedule = [...areaSchedule].sort((a, b) => a.startTime - b.startTime);
+        for (let i = 1; i < sortedSchedule.length; i++) {
+            if (sortedSchedule[i].startTime < sortedSchedule[i - 1].endTime) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
